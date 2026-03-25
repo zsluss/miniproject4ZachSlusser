a530e4cbf5e43f4bcdbb20082257078766d46139
zsluss <zluss@gmail.com>
2026-03-25 10:23:24 -0500

    Add URL routing for weather and create weather models and templates

--- mysite/weather/models.py
+++ mysite/weather/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here..
+
+class Name(models.Model):
+    name = models.CharField(max_length=50)
+    pub_date = models.DateTimeField("time checked")
+
+
+class Zip(models.Model):
+    name = models.ForeignKey(Name, on_delete=models.CASCADE)
+    zipcode = models.IntegerField(max_length=5)
+    pub_date = models.DateTimeField("time checked")
