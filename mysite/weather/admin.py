5444752423e5725cc450cdd9ed6ff5b3fdb4c955
zsluss <zluss@gmail.com>
2026-03-25 10:36:29 -0500

    Refactor admin and models to improve readability and structure

--- mysite/weather/admin.py
+++ mysite/weather/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Name, Zip, WeatherData
 
-# Register your models here.
+
+@admin.register(Name)
+class NameAdmin(admin.ModelAdmin):
+	list_display = ("name", "pub_date")
+	search_fields = ("name",)
+
+
+@admin.register(Zip)
+class ZipAdmin(admin.ModelAdmin):
+	list_display = ("name", "zipcode", "pub_date")
+	search_fields = ("zipcode", "name__name")
+	list_filter = ("pub_date",)
+
+
+@admin.register(WeatherData)
+class WeatherDataAdmin(admin.ModelAdmin):
+	list_display = ("zip", "temperature", "humidity", "condition", "pub_date")
+	search_fields = ("zip__zipcode", "condition")
+	list_filter = ("condition", "pub_date")
--- mysite/weather/models.py
+++ mysite/weather/models.py
@@ -6,11 +6,17 @@ class Name(models.Model):
     name = models.CharField(max_length=50)
     pub_date = models.DateTimeField("time checked")
 
+    def __str__(self):
+        return self.name
+
 
 class Zip(models.Model):
     name = models.ForeignKey(Name, on_delete=models.CASCADE)
     zipcode = models.CharField(max_length=5)
     pub_date = models.DateTimeField("time checked")
+
+    def __str__(self):
+        return f"{self.name} - {self.zipcode}"
 
 class WeatherData(models.Model):
     zip = models.ForeignKey(Zip, on_delete=models.CASCADE)
@@ -18,3 +24,6 @@ class WeatherData(models.Model):
     humidity = models.FloatField()
     condition = models.CharField(max_length=100)
     pub_date = models.DateTimeField("time recorded")
+
+    def __str__(self):
+        return f"{self.zip.zipcode} - {self.condition}"
